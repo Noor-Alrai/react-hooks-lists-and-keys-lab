935ae15bd8e68d8ad2841cf386af0766618b2ccf
Noor Alrai <ameinnoor91@gmail.com>
2023-10-19 14:49:15 +0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const dynamicList = links.map((link, index) =>{
+    return <a key={index} href={`#${link}`}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{dynamicList}</nav>;
 }
 
 export default NavBar;
